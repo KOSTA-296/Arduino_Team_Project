2db212a206058ef9d7c3550fc1f924f26e5b3b7f
Hyunmin <hunmin5928@gmail.com>
2025-04-08 09:48:46 +0900

    디렉토리 정리

--- Arduino/test.ino
+++ Arduino/test.ino
@@ -1,8 +1,9 @@
 #include <SoftwareSerial.h>//소프트웨어 시리얼 라이브러리 추가
 #include <Servo.h>
+#include ""
 
-// #define TXD 2 //TXD를 2번 핀으로 설정
-// #define RXD 3 //RXD를 3번 핀으로 설정
+#define TXD 2 //TXD를 2번 핀으로 설정
+#define RXD 3 //RXD를 3번 핀으로 설정
 #define SPEED_L 6
 #define DC_IN1_L 4
 #define DC_IN2_L 7
@@ -10,11 +11,17 @@
 #define DC_IN1_R 8
 #define DC_IN2_R 12
 #define mySerial Serial
+#define LeftEcho 
+#define LeftTrig
+#define RightEcho
+#define RightTrig
+#define FrontEcho
+#define FrontTrig
 
 Servo myservo;  // 서보 모터 객체 9번핀 사용
 int pos;        // 서보 모터 위치 값을 저장하는 변수
 
-// SoftwareSerial mySerial(TXD, RXD); //소프트웨어 시리얼 mySerial 객체 선언
+SoftwareSerial mySerial(TXD, RXD); //소프트웨어 시리얼 mySerial 객체 선언
 /*
 0 : 정지
 1 : 전진
@@ -41,33 +48,42 @@ void setup() {
   pinMode(SPEED_R, OUTPUT);
   pinMode(DC_IN1_R, OUTPUT);
   pinMode(DC_IN2_R, OUTPUT);
-  // Serial.begin(9600);
+  pinMode()
+  Serial.begin(9600);
 }
 
 void loop() { 
-  if(mySerial.available()) {
-    int command = mySerial.read() - '0';
-
+  // if(mySerial.available()) {
+  if(Serial.available()) {
+    // int command = mySerial.read() - '0';
+    int command = Serial.read() - '0';
     switch(command){
       case 0 :  // 정지 
         Stop();
+        Serial.println("Stop command");
         break;
       case 1 :  // 전진
         Go();
+        Serial.println("Go command");
         break;
       case 2 :  // 후진
         Back();
+        Serial.println("Back command");
         break;
       case 3 :  // 좌회전(전진)
+        Serial.println("Left Go command");
         LeftGo();
         break;
       case 4 :  // 우회전(전진)
+        Serial.println("Right Go command");
         RightGo();
         break;
       case 5 :  // 좌회전(후진)
+        Serial.println("Left Back command");
         LeftBack();
         break;
       case 6 :  // 우회전(후진)
+        Serial.println("Right Back command");
         RightBack();
         break;
       default :
@@ -145,38 +161,38 @@ void RightGo(){
   }
 }
 
-// void LeftBack(){
-//   digitalWrite(DC_IN1_L, LOW);
-//   digitalWrite(DC_IN2_L, HIGH);
-//   digitalWrite(DC_IN1_R, LOW);
-//   digitalWrite(DC_IN2_R, HIGH);
-//   for(pos = 30; pos <= 60; pos += 1){
-//     myservo.write(pos);
-//     delay(15);
-//   }
-//   analogWrite(SPEED_L, 150);
-//   analogWrite(SPEED_R, 150);
-//   delay(3000);
-//   for(pos = 60; pos >= 30; pos -= 1){
-//     myservo.write(pos);
-//     delay(15);
-//   }
-// }
+void LeftBack(){
+  digitalWrite(DC_IN1_L, LOW);
+  digitalWrite(DC_IN2_L, HIGH);
+  digitalWrite(DC_IN1_R, LOW);
+  digitalWrite(DC_IN2_R, HIGH);
+  for(pos = 30; pos <= 60; pos += 1){
+    myservo.write(pos);
+    delay(15);
+  }
+  analogWrite(SPEED_L, 150);
+  analogWrite(SPEED_R, 150);
+  delay(3000);
+  for(pos = 60; pos >= 30; pos -= 1){
+    myservo.write(pos);
+    delay(15);
+  }
+}
 
-// void RightBack(){
-//   digitalWrite(DC_IN1_L, LOW);
-//   digitalWrite(DC_IN2_L, HIGH);
-//   digitalWrite(DC_IN1_R, LOW);
-//   digitalWrite(DC_IN2_R, HIGH);
-//   for(pos = 30; pos >= 0; pos -= 1){
-//     myservo.write(pos);
-//     delay(15);
-//   }
-//   analogWrite(SPEED_L, 150);
-//   analogWrite(SPEED_R, 150);
-//   delay(3000);
-//   for(pos = 0; pos <= 30; pos += 1){
-//     myservo.write(pos);
-//     delay(15);
-//   }
-// }
+void RightBack(){
+  digitalWrite(DC_IN1_L, LOW);
+  digitalWrite(DC_IN2_L, HIGH);
+  digitalWrite(DC_IN1_R, LOW);
+  digitalWrite(DC_IN2_R, HIGH);
+  for(pos = 30; pos >= 0; pos -= 1){
+    myservo.write(pos);
+    delay(15);
+  }
+  analogWrite(SPEED_L, 150);
+  analogWrite(SPEED_R, 150);
+  delay(3000);
+  for(pos = 0; pos <= 30; pos += 1){
+    myservo.write(pos);
+    delay(15);
+  }
+}
